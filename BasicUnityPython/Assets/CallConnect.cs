e4fe3c3918c8e7e3890f363d403d8640588fab1d
NumesSanguis lab <stefstruijk@protonmail.ch>
2017-12-14 14:50:42 +0000

    Successfully send and receive message from Python server to Unity

---
+++ BasicUnityPython/Assets/CallConnect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallConnect : MonoBehaviour {
+
+	public GameObject NetworkSteering;
+
+	// Use this for initialization
+	void Start () {
+		NetworkSteering.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	//	
+	//}
+}
